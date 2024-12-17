207c35a54237e25059d5286baae8e386d55152f6
[PBJ] <[dlthasd1234@naver.com]>
2024-12-17 16:24:07 +0900

    messaage

---
+++ src/main/java/com/example/demo/EasyTravel2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyTravel2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyTravel2Application.class, args);
+	}
+
+}
